0417bcd4fa389855941f5e19cd1601b94e02e089
Bryan Soong <whiterevolver35@gmail.com>
2021-08-25 22:58:18 +0800

    :zap: remove status bar

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:app/screens/home_screen.dart';
 import 'package:app/screens/login_acc_sreen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(App());
 
@@ -20,7 +22,13 @@ class AppHome extends StatefulWidget {
 
 class _AppHomeState extends State<AppHome> {
   @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return LoginScreen();
+    return HomeScreen();
   }
 }
